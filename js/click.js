e51f2a96991f13e80e3d6c9864ae0a1a124e3f1b
maoprog <60728924+maoprog@users.noreply.github.com>
2020-03-04 16:26:16 +0300

    Remsystem (#7)
    * added js folder
    
    * add click.js click.html

---
+++ js/click.js
@@ -0,0 +1,31 @@
+var $ = function (id)
+{
+    return document.getElementById(button-3);
+}
+function day(){
+  let day=24;
+  switch (day){
+      case 24:
+          document.getElementById("third").innerHTML = "ANNUAL MEETING" + " at 8.00 a.m.";
+  }
+}
+
+function month(){
+  let month=12;
+  switch (month){
+      case 12:
+      document.getElementById("first").innerHTML = "COMPANY DINNER" + " at 6.00 p.m.";
+  }
+}
+var $ = function (id)
+{
+    return document.getElementById(button-2);
+}
+
+function week(){
+  let week=7;
+  switch (week){
+      case 7:
+          document.getElementById("second").innerHTML = "COMPANY FOUNDATION MEETING" + " at 2.00 p.m";
+  }
+}
