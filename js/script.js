1dccbb5c4b408e3e87752b2a6810c1d1321b968b
MarkMuniu <nyttocreator@gmail.com>
2020-03-06 09:52:18 +0300

    add toggle effect on the calendar

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $("#clickcal").click(function() {
+    $(".showHide").slideToggle(300);
+  });
+});
--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $('#clickcal').click(function() {
-    $('iframe').slideDown(300);
-    $('iframe').slideUp(300);
-  });
-});
